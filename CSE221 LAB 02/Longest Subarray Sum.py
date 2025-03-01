60459872a3850c462796eb59c3028cc2285b7315
Mazid Zomader <mazidzomader.bracu@gmail.com>
2025-03-02 05:16:39 +0600

    Update Longest Subarray Sum.py

--- CSE221 LAB 02/Longest Subarray Sum.py
+++ CSE221 LAB 02/Longest Subarray Sum.py
@@ -1,15 +1,13 @@
 N, K = map(int, input().split())
 arr = list(map(int, input().split()))
-left = 0  # Start of window
-sum = 0
-sub_arr_len = 0
-
+left, sum, sub_arr_len = 0, 0, 0
+ 
 for index in range(N):  
     sum += arr[index]
-
+ 
     while sum > K:  
         sum -= arr[left]
         left += 1  
-
+ 
     sub_arr_len = max(sub_arr_len, index - left + 1) 
 print(sub_arr_len)
